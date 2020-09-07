e3413d8411e3fc6d32cf4ad5b56cc4c552a25fb6
Edson Ney de Farias <edson.ney@gmail.com>
2020-09-07 20:50:53 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,6 +25,7 @@ public class Program {
 
 		//Testa se a data de checkout não é posterior ao checkin
 		//A data tem um método chamado after que testa se uma data é depois da outra
+		//Esta validação não pode ser aplicada no construtor porque não da para ele retornar um string
 		if (!checkOut.after(checkIn)) {
 			System.out.println("Error in reservation: Check-out date must be after check-in date");
 		}
@@ -40,15 +41,11 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation " + error);
 			} 
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}
 			else {
-				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation " + reservation);
 			}
 		}
